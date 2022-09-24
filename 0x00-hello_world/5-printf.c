c89bbd550ba76e16ffe7c8bf603d61043fb42649
Agwuobinna <agwuobinna29@gmail.com>
2022-09-23 20:18:33 -0700

    printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 /**
- *  main -Print out a string to stdout.
- * Return: Always 0 (Success)
+ * main -Print out a string to stdout.
+ * Return: 0 if Success
  */
 int main(void)
 {
-	printf("\"with proper grammar, but the outcome is a piece of art,\n" );
+	printf("with proper grammar, but the outcome is a piece of art,\n"
+	      );
 		return (0);
 }
