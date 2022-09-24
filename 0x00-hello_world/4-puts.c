88c952f82eb488d4166600fd241d670d253dddb2
Agwuobinna <agwuobinna29@gmail.com>
2022-09-23 19:34:39 -0700

    Program C

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 /**
- * Description: main -Print out a string to stdout.
+ * Description: main -Print output of string to stdout.
  * Return: 0 if success.
  */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle"
-	    ):
-		return (0):
+	    );
+		return (0);
 }
