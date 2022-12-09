0af61487497f3101ddf5702eb7f6fc1ab29b865b
Agwuobinna <agwuobinna29@gmail.com>
2022-12-08 20:08:32 -0800

    Hello World

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,21 +1,21 @@
-#include <stdio.h>                                                                                                                                         
-/**                                                                                                                                                        
- * main -Print the size of various types on the computer.
- * Code by Obinna                                                                                           
- * * Return: 0 if Success                         
- */                                                                                                                                          
-int main(void)                                                                                                                                           
-{  
-		char a;
-		int b;
-		long int c;
-		long long int d;
-		float f;
+#include <stdio.h>
+/**
+ * main  - Print the size of various types on the computer.
+ * Code by Obinna
+ * Return: 0
+ */
+int main(void)
+{
+	char a;
+	int b;
+	long int c;
+	long long int d;
+	float f;
 
-		printf("Size of a char: %1u bytes(S)\n", (unsigned long)sizeof(a);
-		printf("Size of an int: %1u bytes(S)\n", (unsigned long)sizeof(b);
-		printf("Size of a long int: %1u bytes(S)\n", (unsigned long)sizeof(c);    
-		printf("Size of a long |long int: %1u bytes(S)\n", (unsigned long)sizeof(d);                 
-		printf("Size of a float: %1u bytes(S)\n", (unsigned long)sizeof(f);                                                                     
-		return (0);
-}	
+	printf("Size of a char: %1u bytes(S)\n", (unsigned long)sizeof(a));
+	printf("Size of an int: %1u bytes(S)\n", (unsigned long)sizeof(b));
+	printf("Size of a long int: %1u bytes(S)\n", (unsigned long)sizeof(c));
+	printf("Size of a long |long int: %1u bytes(S)\n", (unsigned long)sizeof(d));
+	printf("Size of a float: %1u bytes(S)\n", (unsigned long)sizeof(f));
+	return (0);
+}
