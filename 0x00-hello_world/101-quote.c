df8bbc8e11e99867c97a35892161fd2e17b1e3cc
Agwuobinna <agwuobinna29@gmail.com>
2022-12-08 20:44:13 -0800

    piece of art is useful

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,2 +1,9 @@
-#!/bin/bash
-
+#include <stdio.h>   
+/**
+ * main  - Print and that piece of art is useful" - Dora Korpar, 2015-10-19,
+ * 
+ * Return: 1 
+ */
+{
+	 printf("and that piece of art is useful" - Dora Korpar, 2015-10-19")
+}
